6ff56e67cf83a355907e2eeab77864cca008a921
Tomi Huttula <tomi.huttula@gmail.com>
2013-09-30 21:10:17 -0700

    test

---
+++ www/javascript/index.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+window.onload = init;
+
+function init() {
+//	document.getElementById("description").innerHTML = editor.description;
+//	document.getElementById("post").innerHTML = editor.post;
+//	alert(comics.comicsList[0].comicDataUrl);	
+	}
+	
